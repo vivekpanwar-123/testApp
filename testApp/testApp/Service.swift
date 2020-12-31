d890550b0f102a9e4a3a5fa4efa1e99e36295eb0
Astha Sachdeva <asachdeva@netgear.com>
2020-12-31 11:41:40 +0530

    Finished our Loco Framework

---
+++ testApp/testApp/Service.swift
@@ -0,0 +1,16 @@
+//
+//  Service.swift
+//  testApp
+//
+//  Created by VVDN on 12/31/20.
+//
+
+import Foundation
+public class Service {
+    private init() {}
+    public static func doSomething() -> String {
+        return "My Name is Vivek Panwar"
+        
+    }
+
+}
---
+++ testApp/testApp/testApp.h
@@ -0,0 +1,18 @@
+//
+//  testApp.h
+//  testApp
+//
+//  Created by VVDN on 12/31/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for testApp.
+FOUNDATION_EXPORT double testAppVersionNumber;
+
+//! Project version string for testApp.
+FOUNDATION_EXPORT const unsigned char testAppVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <testApp/PublicHeader.h>
+
+
